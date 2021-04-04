622763e64170092f22c6f70b17be86f6868ad29c
hugo5926 <81909583+hugo5926@users.noreply.github.com>
2021-04-04 14:05:19 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,5 +1,5 @@
 Config = {}
 Config.Buttons = {
-    {index = 0,name = '%Website%',url = 'https://www.google.com'},
-    {index = 1,name = '%Connect%',url = 'fivem://connect/MyIpAddress:30120'}
+    {index = 0,name = 'DISCORD',url = 'https://discord.gg/Tg6vPr2azP'},
+    {index = 1,name = 'SE CONNECTER',url = 'fivem://connect/51.75.236.160:31004'}
 }
